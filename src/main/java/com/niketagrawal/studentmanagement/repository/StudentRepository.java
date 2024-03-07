29a6301f1cbbaef883666ad43c7f92295ca05561
Niket-iiitu <niketagrawal777@gmail.com>
2024-03-07 08:25:22 +0530

    Adding code to attach repository.

--- src/main/java/com/niketagrawal/studentmanagement/repository/StudentRepository.java
+++ src/main/java/com/niketagrawal/studentmanagement/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.niketagrawal.studentmanagement.repository;
 
-public interface StudentRepository {
+import com.niketagrawal.studentmanagement.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
