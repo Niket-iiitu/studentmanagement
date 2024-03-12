e646954498f6b84b931ff491401bf7b6141ddb5f
Niket-iiitu <niketagrawal777@gmail.com>
2024-03-12 07:44:22 +0530

    Added .env and fix an issue causing crash on run.

--- src/main/java/com/niketagrawal/studentmanagement/StudentmanagementApplication.java
+++ src/main/java/com/niketagrawal/studentmanagement/StudentmanagementApplication.java
@@ -2,8 +2,9 @@ package com.niketagrawal.studentmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class StudentmanagementApplication {
 
 	public static void main(String[] args) {
